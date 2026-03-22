403e24651206e561c6899a2437de0b563b18d2a7
Narhuz.fasouy321 <35305@students.riphah.edu.pk>
2026-03-22 05:45:00 +0500

    Update MongoDB connection URI and upgrade flatted package version

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
-
 require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
 connectDB();
